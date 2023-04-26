3e1be6c935b311d576563cb0adea77bc3c9a6338
ENikS <evgeni@eniks.com>
2023-04-26 14:57:55 -0700

    Restore point

--- tests/HAMTrie/TrieTests.cs
+++ tests/HAMTrie/TrieTests.cs
@@ -1,11 +1,35 @@
 ﻿using HAMT;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
 
 namespace CTrie.Tests
 {
     [TestClass]
     public partial class HAMTrieTests
     {
+        [DataTestMethod]
+        [DataRow((uint)0x80000000)]
+        public void Int_hex(uint hex)
+        {
+            var trie = new HAMTrie<int>();
+
+            trie[hex] = 111;
+
+            Assert.AreEqual(111, trie[hex]);
+        }
+
+        [TestMethod]
+        public void Int_01()
+        {
+            var trie = new HAMTrie<uint>();
+
+            trie[0b_11111111_11111111_11111111_11111111] = 111;
+            trie[0b_00000000_00000000_00000000_00000000] = 111;
+
+            Assert.AreEqual(111, trie[0x80000000]);
+        }
+
         [TestMethod]
         public void Int()
         {
